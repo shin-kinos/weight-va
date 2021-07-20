53e94af965087dc5eae5f59b6107dc370b31e096
shin-kinos <kinoshita00000000@gmail.com>
2021-07-20 11:22:10 +0900

    Commented out

--- src/options.rs
+++ src/options.rs
@@ -4,7 +4,7 @@ use std::process;
 
 pub struct Options {
 	pub input    : String,
-	pub tolerate : String,
+	//pub tolerate : String,
 }
 
 impl Options {
@@ -14,7 +14,7 @@ impl Options {
 		let argc : usize = argv.len();
 
 		let mut arg_i : &String = &String::new();
-		let mut arg_t : &String = &String::from( "yes" );
+		//let mut arg_t : &String = &String::from( "yes" );
 
 		if argc < 3 { show_usage(&argv[0]) };
 
@@ -22,21 +22,23 @@ impl Options {
 		while i < argc {
 			match argv[i].as_str() {
 				"-i" => { i += 1; arg_i = &argv[i]; },
-				"-t" => { i += 1; arg_t = &argv[i]; }
+				//"-t" => { i += 1; arg_t = &argv[i]; }
 				"-h" => { show_usage(&argv[0]); },
 				_    => { show_usage(&argv[0]); },
 			}
 			i += 1;
 		}
 
+		/*
 		match ( *arg_t ).as_str() {
 			"yes" | "no" => (),
 			_            => show_usage( &argv[0] ),
 		}
+		*/
 
 		Options {
 			input    : arg_i.to_string(),
-			tolerate : arg_t.to_string(),
+			//tolerate : arg_t.to_string(),
 		}
 	}
 
@@ -45,7 +47,7 @@ impl Options {
 		println!( "\nParameter set:" );
 		println!( "===========================================" );
 		println!( "Input filename  : {}", self.input );
-		println!( "Tolerate BZXU   : {}", self.tolerate );
+		//println!( "Tolerate BZXU   : {}", self.tolerate );
 		println!( "===========================================" );
 	}
 }
@@ -54,7 +56,7 @@ fn show_usage( arg : &String )
 {
 	println!( "Usage: {} [Options] \n\nOptions\n\n", *arg );
 	println!( "   -i\tInput filename in aligned Multi-FASTA format, REQUIRED." );
-	println!( "   -t\tTolerate AA types 'B', 'Z', 'X' and 'U' in input file ('yes' or 'no', default 'yes').\n    \tIf 'no', program halts when the input file includes B, Z, X, or U." );
+	//println!( "   -t\tTolerate AA types 'B', 'Z', 'X' and 'U' in input file ('yes' or 'no', default 'yes').\n    \tIf 'no', program halts when the input file includes B, Z, X, or U." );
 	println!( "   -h\tPrint this help, ignore all other arguments." );
 	println!( "\n" );
 
